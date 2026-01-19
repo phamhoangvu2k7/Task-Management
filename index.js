fce7c05bc5af45cd1ddcf7ed27369a8d9f4d1e41
Pham Hoang Vu <phamvuhoang486@gmail.com>
2026-01-19 14:59:50 +0700

    update: take task detail

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-console.log("OK");
+const express = require("express");
+const database = require("./config/database");
+require("dotenv").config();
+
+database.connect();
+
+const Task = require("./model/task_model");
+
+const app = express();
+const port = process.env.PORT;
+
+app.get("/task", async (req, res) => {
+    const tasks = await Task.find({
+        deleted: false
+    });
+
+    res.json(tasks);
+});
+
+app.get("/task/detail/:id", async (req, res) => {
+    const id = req.params.id;
+
+    const task = await Task.findOne({
+        _id: id,
+        deleted: false
+    });
+
+    res.json(task);
+});
+
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`);
+});
