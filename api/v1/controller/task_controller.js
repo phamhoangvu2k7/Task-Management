cc0cb75b30551d84a1a0b180f66d659ae6895b33
Pham Hoang Vu <phamvuhoang486@gmail.com>
2026-01-21 13:58:38 +0700

    feat: pagination

--- api/v1/controller/task_controller.js
+++ api/v1/controller/task_controller.js
@@ -1,4 +1,5 @@
 const Task = require("../model/task_model");
+const paginationHelper = require("../../../helpers/pagination");
 
 // [GET] /api/v1/tasks
 module.exports.index = async (req, res) => {
@@ -9,13 +10,31 @@ module.exports.index = async (req, res) => {
     if (req.query.status) {
         find.status = req.query.status;
     }
+    // Pagination
+    let initPagination = {
+        currentPage: 1,
+        limitItems: 2
+    };
+    const countTasks = await Task.countDocuments(find);
+    const objectPagination = paginationHelper(
+        initPagination,
+        req.query,
+        countTasks
+    )
 
+    // End Pagination
+
+    // Sort
     const sort = {};
     if (req.query.sortKey && req.query.value) {
         sort[req.query.sortKey] = req.query.value;
     }
+    //End Sort
 
-    const tasks = await Task.find(find).sort(sort);
+    const tasks = await Task.find(find)
+        .sort(sort)
+        .limit(objectPagination.limitItems)
+        .skip(objectPagination.skip)
 
     res.json(tasks);
 }
