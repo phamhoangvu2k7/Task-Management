4a0b2a391af6434d0222c73ae33b90cfa026caf2
Pham Hoang Vu <phamvuhoang486@gmail.com>
2026-02-17 17:17:49 +0700

    feat: create swagger

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const bodyParser = require("body-parser");
 const database = require("./config/database");
 const cookieParser = require("cookie-parser");
 
+// 1. Import thư viện Swagger (Thêm ở đầu file)
+const swaggerUi = require('swagger-ui-express'); 
+const YAML = require('yamljs');                  
+
 require("dotenv").config();
 
 database.connect();
@@ -16,8 +20,13 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(cors());
 
+// 2. Load file YAML và Cấu hình đường dẫn xem tài liệu (Thêm trước dòng route(app))
+const swaggerDocument = YAML.load('./swagger.yaml');           
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument)); 
+
 route(app);
 
 app.listen(port, () => {
-    console.log(`App listening on port ${port}`);
+  console.log(`App listening on port ${port}`);
+  console.log(`Swagger Docs available at http://localhost:${port}/api-docs`); // <--- (Tùy chọn) Thêm dòng này để dễ click
 });
