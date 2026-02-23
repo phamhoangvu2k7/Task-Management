675c978f541d0a0b06c8851479bb727597c06cb2
Pham Hoang Vu <phamvuhoang486@gmail.com>
2026-02-23 21:15:30 +0700

    update: install library

---
+++ index.ts
@@ -0,0 +1,25 @@
+// const cors = require("cors");
+// const express = require("express");
+// const bodyParser = require("body-parser");
+// const database = require("./config/database");
+// const cookieParser = require("cookie-parser");
+
+// require("dotenv").config();
+
+// database.connect();
+
+// const route = require("./api/v1/routes/index_route");
+// const app = express();
+// const port = process.env.PORT;
+
+// app.use(cookieParser());
+// app.use(bodyParser.json());
+// app.use(cors());
+
+// route(app);
+
+// app.listen(port, () => {
+//     console.log(`App listening on port ${port}`);
+// });
+
+console.log("OK");
